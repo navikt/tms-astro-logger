81609a25c4bc2ddfa26aa826d3d1c8a7f986caee
sasoria <sergio.alberto.arevalo.soria@nav.no>
2026-07-02 12:37:45 +0200

    Refactor

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-    entry: ['src/index.ts'],
+    entry: ['package/index.ts'],
     format: 'esm',
     dts: true,
     sourcemap: true,
